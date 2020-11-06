92795f4694d20f5fa6d34380a7893838e4a70687
Pedro Roccon <pedro@pedroroccon.com.br>
2020-11-06 18:29:57 +0000

    Criando o model Tenant e seus recursos

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use App\Models\Tenant;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -23,10 +24,12 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
+            'tenant_id' => Tenant::factory(), 
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
+            'role' => 'admin', 
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'), 
             'remember_token' => Str::random(10),
         ];
     }
